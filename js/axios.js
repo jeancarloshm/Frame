e16073d6e535325e63b415c52a3e6fe31b8da3db
Enmanuel Reynoso <enmanueldev@gmail.com>
2022-12-02 15:43:58 -0400

    Added files and API related code

--- js/axios.js
+++ js/axios.js
@@ -19,25 +19,42 @@
 
  const searchMovie =  async (searchInput) => {
     try { 
-     const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=4a24d8326eef858419a61cb94a02d429&language=en-US&query=${searchInput}&page=1&include_adult=false `)
-     console.log(response.data.results)
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=4a24d8326eef858419a61cb94a02d429&language=en-US&query=${searchInput}&page=1&include_adult=false `)
+      .then((response) => {
+        console.log(response);
+        let movies = response.data.results;
+        let displayMovies = '';
+        $.each(movies, (index, movie) => {
+          displayMovies += `
+            <div class="col-md-3">
+              <div class="well text-center">
+                <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}">
+                <h5>${movie.title}</h5>
+                <p class ="movie-overview">${movie.overview}</p>
+              </div>
+            </div>
+          `;
+        });
+        
+        $('#movies').html(displayMovies);
+      })
     }catch(error) {
      console.log(error)
     }
 }
 
-const getPopularMovies = async() => {
-    try {
-        const response = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=4a24d8326eef858419a61cb94a02d429&language=en-US&page=1')
-        console.log(response.data.results) 
+// const getPopularMovies = async() => {
+//     try {
+//         const response = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=4a24d8326eef858419a61cb94a02d429&language=en-US&page=1')
+//         console.log(response.data.results) 
 
-    } catch (error) {
-        console.log(error)
-    }
+//     } catch (error) {
+//         console.log(error)
+//     }
     
-}
+// }
 
-getPopularMovies()
+// getPopularMovies()
 searchMovie()
 
 
