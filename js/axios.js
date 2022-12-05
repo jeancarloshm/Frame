f699bed53f9761020d7466cc9d1f5fad06180fb3
Jeancarlos Herrera <jeancarlos.herrera113@gmail.com>
2022-12-05 17:19:52 -0400

    Cleaning the code

--- js/axios.js
+++ js/axios.js
@@ -1,16 +1,4 @@
-// const dotEnv = require('dotenv')
-// dotEnv.config({path:'dotenv.config'})
 
-//event for when the form is submitted//
-// const searchForm = document.getElementById('search-form')
-// searchForm.addEventListener('submit', (e) => {
-//  console.log('searchInput'.val())
-//  e.preventDefault();
-//  })
-
-//event on submit on jqeuery
-
-//Inicio de JQuery
 
 $(document).ready(() => {
   $(`#search-form`).on(`submit`, (e) => {
@@ -34,6 +22,7 @@ $(document).ready(() => {
   //Renderizar las peliculas cuando se busquen
   const searchMovie = async (searchInput) => {
     try {
+
       //Llama a el metodo ObtainData para obtener las diferentes peliculas (Superman, Batman, Spiderman ..... )
       const results = await obtainData(searchInput);
       let displayMovies = [];
@@ -50,10 +39,8 @@ $(document).ready(() => {
         </div>
           `;
       });
-
       
-      //Si le da click a un boton con el ID: Add-Boton que realice la funcion (EN MANTENIMIENTO!!!!!! )
-      
+    //Si le da click a un boton con el ID: Add-Boton que realice la funcion (EN MANTENIMIENTO!!!!!! )
     $("#movies").on("click", "#add-button", (e) => {
     const movieElement = $(e.target).closest(".col-md-3")
     const title = movieElement.find("h4").text()
@@ -75,81 +62,19 @@ $(document).ready(() => {
       releaseDate: releaseDate,
     }
 
-    
-   
-    
     newWatchlist.push(movieWatchlist)
     localStorage.setItem('movieInWatch' ,JSON.stringify(newWatchlist))
     console.log(movieWatchlist)
    })
-  
-
-  //Si le da click a un boton con el ID: Add-Boton que realice la funcion (EN MANTENIMIENTO!!!!!! )
-      
-  // function addToWatchlist(id) {
-  //   const movie = results.find(m => m.id === id) 
-  //   const displayToWatch =  {
-  //   id: movie.id,
-  //   poster: movie.poster_path,
-  //   title: movie.title,
-  //   year: movie.release_date,
-  //   }
-  //   const currentWatchlist = localStorage.getItem('watchList') || '[]'
-  //   const newWatchlist = JSON.parse(currentWatchlist)
-  //   newWatchlist.push(displayToWatch)
-  //   localStorage.setItem('watchList' ,JSON.stringify(newWatchlist))
-  //  };
-
-      
-  //  function displayData() {
-  //   var data = localStorage.getItem('movieInWatch');
-  //   if (data) {
-  //   data = JSON.parse(data);
-  //   $('#selected-movie').html(''); // clear previous data
-  //   for (var i = 0; i < data.length; i++) {
-  //   $('#selected-movie').append(`
-  //        <div class="col-md-3"> 
-  //         <div class="well text-center">
-  //         <img src="https://image.tmdb.org/t/p/original${data[i].posterPath}">
-  //           <h4>${data[i].title}</h4>
-  //           <h5>${data[i].releaseDate}<h5>
-  //           <h6 hidden>${data[i].id}</h6>
-  //         </div>
-  //       </div>
-  //   `);
-  //   $("selected-movie").html(displayData());
-  //   }
-  //   }
-  //   }
-    
-  //   displayData();
 
    $("#movies").html(displayMovies);
 
-
-
-      //$("#selected-movie").html(selectedMovie);
     } 
     catch (error) {
       console.log(error);
     }
   };
-
-  // function displayToWatchList() {
-  //   let selectedMovie = JSON.parse(localStorage.getItem('movieInWatch'));
-  //   if (selectedMovie) {
-  //     `<div class="col-md-3">
-  //     <div class="well text-center">
-  //      <img src="https://image.tmdb.org/t/p/original${posterPath}">
-  //       <h5>${title}</h5>
-  //       <h4>${releaseDate}}<h4>
-  //     </div>
-  //   </div>`;
-  //   }
-  //   $("#selected-movie").html(displayToWatchList());
-
-  // }  
-
+  
    const getPopularMovies = async() => {
        try {
            const response = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=4a24d8326eef858419a61cb94a02d429&language=en-US&page=1')
@@ -192,9 +117,6 @@ $(document).ready(() => {
           posterPath: posterPath,
           releaseDate: releaseDate,
         }
-    
-        
-       
         
         newWatchlist.push(movieWatchlist)
         localStorage.setItem('movieInWatch' ,JSON.stringify(newWatchlist))
@@ -204,7 +126,6 @@ $(document).ready(() => {
        } catch (error) {
            console.log(error)
        }
-
    }
 
    getPopularMovies()
--- js/watchlist.js
+++ js/watchlist.js
@@ -1,27 +1,4 @@
  
- 
-  // function displayData() {
-  //   var data = localStorage.getItem('movieInWatch');
-  // console.log(data);
-  //   if (data) {
-  //   // data = JSON.parse(data);
-  //   // $('#selected-movie').html(''); // clear previous data
-  //   for (var i = 0; i < data.length; i++) {
-    // $('#selected-movie').append(`
-  //        <div class="col-md-3"> 
-  //         <div class="well text-center">
-  //         <img src="https://image.tmdb.org/t/p/original${data[i].posterPath}">
-  //           <h4>${data[i].title}</h4>
-  //           <h5>${data[i].releaseDate}<h5>
-  //           <h6 hidden>${data[i].id}</h6>
-  //         </div>
-  //       </div>
-  //   `);
-  //   // $("selected-movie").html(displayData());
-  //   }
-  //   }
-  //   }
-  //   displayData();
 
   $(document).ready(function(){
     var data = localStorage.getItem('movieInWatch');
@@ -45,39 +22,4 @@
 });
 
     
-    // console.log(x);
-    // $("#selected-movie").html(x);
-
-
-  //   const displayData = async() => {
-  //     try {
-        
-  //       var data = localStorage.getItem('movieInWatch');
-  //       JSON.parse(data)
-  //       console.log(data);
-  //       if (data != ' ') {    
-  //         let displayMovies;  
-  //         for (var i = 0; i < data.length; i++) {
-  //         displayMovies += `
-  //        <div class="col-md-3"> 
-  //        <div class="well text-center">
-  //        <img src="https://image.tmdb.org/t/p/original${data[i].poster_path}"></a>
-  //          <h4>${data[i].title}</h4>
-  //          <h5>${data[i].release_date}</h5>
-  //          <h6 hidden>${data[i].id}</h6>
-  //          <a class="btn btn-primary" id="add-button" >Add to watchlist</a>
-  //        </div>
-  //      </div>
-  //        `;
-  //    };
-
-  //    $("#selected-movie").html(displayMovies);
-          
-  //   }
-  //     } catch (error) {
-  //         console.log(error)
-  //     }
-    
-  //   }
-  
-  // displayData();
+   
