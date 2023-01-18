ce006071a87c7f1a294e55c8d48d12585ad21604
Enmanuel Reynoso <enmanueldev@gmail.com>
2023-01-18 04:47:03 -0400

    refactoring

--- js/axios.js
+++ js/axios.js
@@ -8,7 +8,7 @@ $(document).ready(() => {
   });
 
   //Metodo que invoca los datos en AXIOS mediante el Search Input 
-  const obtainData = async (searchInput) => {
+const obtainData = async (searchInput) => {
     try {
       const {data: { results }} = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=4a24d8326eef858419a61cb94a02d429&language=en-US&query=${searchInput || ''}&page=1&include_adult=false `
       );
@@ -17,9 +17,34 @@ $(document).ready(() => {
       console.log(error);
     }
   };
+  
+const addToWatchlist = (e) => {
+    const movieElement = $(e.target).closest(".col-md-3");
+    const title = movieElement.find("h4").text();
+    const id = movieElement.find('h6').text();
+    const posterPath = movieElement.find("img").attr("src");
+    const releaseDate = movieElement.find("h5").text();
+
+    const currentWatchlist = JSON.parse(localStorage.getItem('movieInWatch')) || [];
+
+    if (!currentWatchlist.find(m => m.id === id)) {
+        const movieWatchlist = {
+            id,
+            title,
+            posterPath,
+            releaseDate,
+        };
+
+        currentWatchlist.push(movieWatchlist);
+        localStorage.setItem('movieInWatch', JSON.stringify(currentWatchlist));
+        console.log(movieWatchlist);
+    } else {
+        alert('Movie already in watchlist!');
+    }
+}
 
   //Renderizar las peliculas cuando se busquen
-  const searchMovie = async (searchInput) => {
+const searchMovie = async (searchInput) => {
     try {
 
       //Llama a el metodo ObtainData para obtener las diferentes peliculas 
@@ -42,40 +67,20 @@ $(document).ready(() => {
       
     //Si le da click a un boton con el ID: Add-Boton que realice la funcion 
     $("#movies").on("click", "#add-button", (e) => {
-    const movieElement = $(e.target).closest(".col-md-3")
-    const title = movieElement.find("h4").text()
-    const id = movieElement.find('h6').text()
-    console.log(id)
-    const posterPath = movieElement.find("img").attr("src")
-    const releaseDate = movieElement.find("h5").text()
+      addToWatchlist(e)
+    });
     
-    const currentWatchlist = localStorage.getItem('movieInWatch') || '[]'
-    const newWatchlist = JSON.parse(currentWatchlist)
-     if (newWatchlist.find(m => m.id === id)){
-      return 
-     }
-     
-    const movieWatchlist = {
-      id: id,
-      title: title,
-      posterPath: posterPath,
-      releaseDate: releaseDate,
-    }
-
-    newWatchlist.push(movieWatchlist)
-    localStorage.setItem('movieInWatch' ,JSON.stringify(newWatchlist))
-    console.log(movieWatchlist)
-   })
-
-   $("#movies").html(displayMovies);
-
+    $("#movies").html(displayMovies)
     } 
     catch (error) {
       console.log(error);
     }
   };
   
-   const getPopularMovies = async() => {
+
+
+  
+const getPopularMovies = async() => {
        try {
            const response = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=4a24d8326eef858419a61cb94a02d429&language=en-US&page=1')
            console.log(response.data.results)
@@ -95,34 +100,10 @@ $(document).ready(() => {
           `;
       });
 
-      $("#popular-movies").html(displayPopular);
-
       $("#popular-movies").on("click", "#add-button", (e) => {
-        const movieElement = $(e.target).closest(".col-md-3")
-        const title = movieElement.find("h4").text()
-        const id = movieElement.find('h6').text()
-        console.log(id)
-        const posterPath = movieElement.find("img").attr("src")
-        const releaseDate = movieElement.find("h5").text()
-        
-        const currentWatchlist = localStorage.getItem('movieInWatch') || '[]'
-        const newWatchlist = JSON.parse(currentWatchlist)
-         if (newWatchlist.find(m => m.id === id)){
-          return 
-         }
-         
-        const movieWatchlist = {
-          id: id,
-          title: title,
-          posterPath: posterPath,
-          releaseDate: releaseDate,
-        }
-        
-        newWatchlist.push(movieWatchlist)
-        localStorage.setItem('movieInWatch' ,JSON.stringify(newWatchlist))
-        console.log(movieWatchlist)
+        addToWatchlist(e)
        })
-
+       $("#popular-movies").html(displayPopular);
        } catch (error) {
            console.log(error)
        }
--- js/gqlserver.js
+++ js/gqlserver.js
@@ -24,31 +24,12 @@ type Query {
 // The root provides a resolver function for each API endpoint
 var root = {
     Query: {
-        async searchMovies(_, {searchInput}) {
-            try {
-                // Make the API request
-                const { data: { results } } = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=4a24d8326eef858419a61cb94a02d429&language=en-US&query=${searchInput}&page=1&include_adult=false`);
-                // Create an empty array to store the movie objects
-                let movieArr = [];
-                if(!results.length) return movieArr;
-                // Iterate over the results and push the movie objects into the array
-                results.forEach(movie => {
-                    movieArr.push({
-                        id: movie.id,
-                        title: movie.title,
-                        poster_path: movie.poster_path,
-                        release_date: movie.release_date
-                    });
-                });
-                // Return the array of movie objects
-                return movieArr;
-            } catch (error) {
-                console.log(error);
-                return error;
-            }
-        }
-    },
-async popularMovies() {
+        async searchMovies(_, { searchInput }) {
+          const { data: { results } } = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=4a24d8326eef858419a61cb94a02d429&language=en-US&query=${searchInput}&page=1&include_adult=false`);
+          return results.filter(movie => movie.id && movie.title && movie.poster_path && movie.release_date);
+    }
+},
+    async popularMovies() {
             const { data: { results } } = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=4a24d8326eef858419a61cb94a02d429&language=en-US&page=1');
             return results.map(movie => ({
                 id: movie.id,
