5965117b77fc04a54e3a34f3dd302ffa8d40b4ed
EliardoB <115761583+EliardoB@users.noreply.github.com>
2023-07-17 01:15:42 -0400

    Still trying

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,7 @@
 const jestDom = require('@testing-library/jest-dom');
 const { TextEncoder, TextDecoder } = require('text-encoding');
+const $ = require('jquery');
 
+global.$ = $;
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
