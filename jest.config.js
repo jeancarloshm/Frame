77f16f7019c3153fc00480f94fd7c9f091eda8ef
EliardoB <115761583+EliardoB@users.noreply.github.com>
2023-07-17 00:59:02 -0400

    Trying jest

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,5 @@ module.exports = {
     coverageDirectory: 'coverage',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     testEnvironment: 'jest-environment-jsdom',
-    "type": "module",
-    transformIgnorePatterns: [
-        'node_modules/(?!(@testing-library/jest-dom)/)',
-      ],
 
   };
