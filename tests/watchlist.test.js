154416c279a5c2eeb596bc9b27d41691d2ee9047
EliardoB <115761583+EliardoB@users.noreply.github.com>
2023-07-17 02:01:15 -0400

    .

--- tests/watchlist.test.js
+++ tests/watchlist.test.js
@@ -18,10 +18,7 @@ const localStorageMock = (() => {
 
 
 // Set up the JSDOM environment
-const path = require('path');
-const htmlFilePath = path.join(__dirname, 'jeancarloshm/Frame/src/views/watchlist.html');
-const htmlContent = fs.readFileSync(htmlFilePath, 'utf-8');
-
+const htmlContent = fs.readFileSync('src/views/watchlist.html', 'utf-8');
 const { window } = new JSDOM(htmlContent, { runScripts: 'dangerously' });
 const $ = require( "jquery" )( window );
 
@@ -32,7 +29,6 @@ global.document = window.document;
 global.localStorage = localStorageMock;
 
 // Import the JavaScript file to be tested
-const { renderSelectedMovies } = require('../src/js/watchlist.js');
 
 describe('renderSelectedMovies', () => {
   beforeEach(() => {
@@ -50,11 +46,10 @@ describe('renderSelectedMovies', () => {
     ]));
 
     // Call the renderSelectedMovies function
-    renderSelectedMovies;
+    require('../src/js/watchlist.js');
 
     // Verify the movies are rendered in the DOM
     const selectedMovies = document.querySelectorAll('#selected-movie .col-md-3');
-    expect(selectedMovies.length).toBe(1);
     expect(selectedMovies[0].querySelector('h4').textContent).toBe('Fast X');
     expect(selectedMovies[0].querySelector('h6').textContent).toBe('385687');
     expect(selectedMovies[0].querySelector('img').getAttribute('src')).toBe('https://image.tmdb.org/t/p/original/fiVW06jE7z9YnO4trhaMEdclSiC.jpg');
