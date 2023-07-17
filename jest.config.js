87718aca5d04e05ecdcf41b1981261ecb4212fa4
EliardoB <115761583+EliardoB@users.noreply.github.com>
2023-07-17 00:30:33 -0400

    .

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
     collectCoverage: true,
     collectCoverageFrom: ['src/**/*.{js,jsx}'],
     coverageDirectory: 'coverage',
-    testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     testEnvironment: 'jest-environment-jsdom',
   };
