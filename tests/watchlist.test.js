fee47cc88f54745830fff40c7601f9c433081ea5
EliardoB <115761583+EliardoB@users.noreply.github.com>
2023-07-17 09:06:24 -0400

    a

--- tests/watchlist.test.js
+++ tests/watchlist.test.js
@@ -1,21 +1,4 @@
 
-const { renderSelectedMovies } = require('../src/js/watchlist.js');
-
-describe('renderSelectedMovies', () => {
-  it('renders the movies in the DOM', () => {
-    const data = [
-      {
-        id: '385687',
-        title: 'Fast X',
-        posterPath: 'https://image.tmdb.org/t/p/original/fiVW06jE7z9YnO4trhaMEdclSiC.jpg',
-      },
-    ];
-    renderSelectedMovies(data);
-    expect(screen.getByText('Fast X')).toBeInTheDocument();
-
-  });
-});
-
 // Import the necessary modules and the function to be tested
 const { assert } = require('chai');
 const { JSDOM } = require('jsdom');
