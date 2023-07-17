942f2f004f4b4a12c38b4e78995e675d2351d7b2
EliardoB <115761583+EliardoB@users.noreply.github.com>
2023-07-17 00:51:27 -0400

    ?

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,8 @@ module.exports = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     testEnvironment: 'jest-environment-jsdom',
     "type": "module",
-    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+    transformIgnorePatterns: [
+        'node_modules/(?!(@@testing-library/jest-dom)/)',
+      ],
 
   };
