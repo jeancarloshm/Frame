de1602d3f07acdc7fd2f3e43feb620c949c65173
Jeancarlos Herrera <jeancarlos.herrera113@gmail.com>
2023-07-16 21:49:11 -0400

    fixing errors

--- tests/performance-test.js
+++ tests/performance-test.js
@@ -1,34 +1,31 @@
 const chromeLauncher = require('chrome-launcher');
 const puppeteer = require('puppeteer');
 const lighthouse = require('lighthouse');
-const http = require('http'); // Require 'http' module for the local server
+const http = require('http');
 
 (async () => {
-  const port = 41421; // Replace with your desired port number
+  const port = 3000;
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
 
-  // Start a local server (Outside page.evaluate())
   const server = http.createServer((req, res) => {
     res.end('Hello, World!');
   });
-  server.listen(3000);
+  server.listen(port);
 
-  // Get the local server URL
-  const serverURL = `http://localhost:3000`;
+  const serverURL = `http://localhost:${port}`;
 
   try {
-    const options = { port };
+    const options = { port: port };
     const lighthouseResult = await lighthouse(serverURL, options);
 
-    // Output the performance results
     console.log('Performance score:', lighthouseResult.lhr.categories.performance.score);
     console.log('Loading speed (First Contentful Paint):', lighthouseResult.lhr.audits['first-contentful-paint'].numericValue);
   } catch (error) {
     console.error('Error occurred:', error);
   } finally {
-    // Close the browser and stop the local server
     await browser.close();
     server.close();
   }
-})();
+});
+
