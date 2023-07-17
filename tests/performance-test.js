5f2cd0c820c7e9e61325b752e804b3ea61d1a727
EliardoB <115761583+EliardoB@users.noreply.github.com>
2023-07-16 20:42:25 -0400

    remade

--- tests/performance-test.js
+++ tests/performance-test.js
@@ -1,20 +1,17 @@
 const puppeteer = require('puppeteer');
 const lighthouse = require('lighthouse');
-const { URL } = require('url');
+const http = require('http'); // Require 'http' module for the local server
 
 (async () => {
   const port = 41421; // Replace with your desired port number
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
 
-  // Start a local server
-  const server = await page.evaluate(() => {
-    const server = http.createServer((req, res) => {
-      res.end('Hello, World!');
-    });
-    server.listen(3000);
-    return server;
+  // Start a local server (Outside page.evaluate())
+  const server = http.createServer((req, res) => {
+    res.end('Hello, World!');
   });
+  server.listen(3000);
 
   // Get the local server URL
   const serverURL = `http://localhost:3000`;
